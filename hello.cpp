15db4ea63d6a4252d0c1315cd7a9814f86a14362
Aman-Papaney <amanpapaney2003@gmail.com>
2023-11-04 11:15:11 +0530

    hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+// iostream to use input and output
+# include <iostream>
+
+// a namespace may contain definitions of class, function, etc. that we are going to use here
+// else we have to use them like namespace::functionName eg. std::cout, std::string
+using namespace std;
+
+// all c++ programs start execution from main function
+int main(int argc, char const *argv[])
+{
+    cout << "HELLO WORLD !!" << endl;
+    return 0;
+}
