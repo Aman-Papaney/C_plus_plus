1bb749aada6ab48e103445548ff4ff6a838a9b1f
Aman-Papaney <amanpapaney2003@gmail.com>
2023-11-04 23:06:40 +0530

    Added files

---
+++ patterns_1.cpp
@@ -0,0 +1,41 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{   
+    cout << "PATTERN 1" << endl;
+    // # # # #
+    // # # # #
+    // # # # #
+    // # # # #
+    int row = 4;
+    while (row > 0){
+        int col = 4;
+        while(col > 0){
+            cout << "# ";
+            col--;
+        }
+        cout << endl;
+        row--;
+    }
+
+    cout << "PATTERN 2" << endl;
+    // 1 1 1 1
+    // 2 2 2 2
+    // 3 3 3 3
+    // 4 4 4 4
+    row = 4; 
+    int count = 0;
+    while (row > 0){
+        int col = 4;
+        count++;
+        while (col > 0){
+            cout << count << " ";
+            col--;
+        }
+        cout << endl;
+        row--;
+    }
+
+    return 0;
+}
