efd67adf61e3898dd1fd4fa1dfe524d51b95b67c
Aman-Papaney <amanpapaney2003@gmail.com>
2023-11-25 20:10:23 +0530

    Updated Files

--- Arrays/intro.cpp
+++ Arrays/intro.cpp
@@ -1,6 +1,10 @@
 # include <iostream>
 using namespace std;
 
+// Arrays are data structures that are used to store data without the need to create variable for each value.
+// Arrays only store same type of data. Ex - a int type array will only store integer vaules.
+// It stores values in contiguous(continuous) memory locations.
+
 int main()
 {
     /* code */
